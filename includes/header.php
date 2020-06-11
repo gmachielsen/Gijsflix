17e4b5c0b48db51018a04d3545c90c0618812b27
Gijs Machielsen <g.machielsen@gmail.com>
2020-06-11 16:57:34 +0200

    seriespage and watchpage

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,10 @@ require_once("includes/classes/PreviewProvider.php");
 require_once("includes/classes/CategoryContainers.php");
 require_once("includes/classes/Entity.php");
 require_once("includes/classes/EntityProvider.php");
+require_once("includes/classes/ErrorMessage.php");
+require_once("includes/classes/SeasonProvider.php");
+require_once("includes/classes/Season.php");
+require_once("includes/classes/Video.php");
 
 
 if(!isset($_SESSION["userLoggedIn"])) {
@@ -15,7 +19,7 @@ $userLoggedIn = $_SESSION["userLoggedIn"];
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Welcome to Reeceflix</title>
+        <title>Welcome to Gijsflix</title>
         <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         <script src="https://kit.fontawesome.com/06a651c8da.js" crossorigin="anonymous"></script>
