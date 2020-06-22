771277b7fba6b9edc06b06b20db7a364657fa36f
Gijs Machielsen <g.machielsen@gmail.com>
2020-06-22 12:28:03 +0200

    profile and search page

---
+++ profile.php
@@ -0,0 +1,36 @@
+<?php
+require_once("includes/header.php");
+?>
+
+<div class="settingsContainer column">
+    <div class="formSection">
+      <form method="POST">
+        <h2>User details</h2>
+
+        <?php
+          $user = new User($con, $userLoggedIn);
+
+          $firstName = isset($_POST["firstName"]) ? $_POST["firstName"] : $user->getFirstName();
+          $lastName = isset($_POST["lastName"]) ? $_POST["lastName"] : $user->getLastName();
+          $email = isset($_POST["email"]) ? $_POST["email"] : $user->getEmail();
+        ?>
+
+        <input type="text" name="firstName" placeholder="First name" value="<?php echo $firstName; ?>">
+        <input type="text" name="lastName" placeholder="Last name" value="<?php echo $lastName; ?>">
+        <input type="email" name="email" placeholder="Email" value="<?php echo $email; ?>">
+
+        <input type="submit" name="saveDetailsButton" value="Save">
+      </form>
+    </div>
+
+    <div class="formSection">
+      <form method="POST">
+        <h2>Update password</h2>
+        <input type="password" name="oldPassword" placeholder="Old password" value="">
+        <input type="password" name="newPassword" placeholder="New password" value="">
+        <input type="password" name="newPassword2" placeholder="Confirm new password" value="">
+
+        <input type="submit" name="savePasswordButton" value="Save">
+      </form>
+    </div>
+</div>
